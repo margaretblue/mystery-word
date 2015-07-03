231d306bf5abefd607b56527a723a6bcc528cf85
MARGARET MORRIS <margaretblue@MARGARETs-MacBook-Pro.local>
2015-07-03 02:37:18 -0700

    all mode_words() functions return correct word subset list, 3 tests passing

--- mystery_word.py
+++ mystery_word.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 
 def read_file(textfile):
@@ -18,32 +19,51 @@ def read_file(textfile):
     return word_list
 
 
-def mode_selector():
-    """Asks user to select easy (4-6chars), normal(6-8) or hard(8+) mode and returns mode name"""
+def size_selector():
+    """Asks user to select easy (4-6), normal(6-8) or hard(8+) mode and returns randomized number of chars"""
     user_mode = 0
     while user_mode == 0:
         user_selection = input("Welcome to Mystery Word!\n Do you want to play in (E)ASY, (N)ORMAL, or (H)ARD mode? > ")
         if user_selection.lower() == "e"  or user_selection.lower() == "easy":
             user_mode = "easy"
+            easy_words(word_list)
         elif user_selection.lower() == "n" or user_selection.lower() == "normal":
+            medium_words(word_list)
             user_mode = "normal"
         elif user_selection.lower() == "h" or user_selection.lower() == "hard":
+            hard_words(word_list)
             user_mode = "hard"
         else:
             user_mode = 0
     return user_mode
 
-
 def easy_words(word_list):
-    pass
+    """Given word list, returns a subset list of those words 4-6 characters in size"""
+    #char_size = random.randrange(4, (6+1), 1)
+    mini_word_list = []
+    for word in word_list:
+        if len(word) >= 4 and len(word) <= 6:
+            mini_word_list.append(word)
+    return mini_word_list
 
 
 def medium_words(word_list):
-    pass
+    """Given word list, returns a subset list of those words 6-8 characters in size"""
+    mini_word_list = []
+    for word in word_list:
+        if len(word) >= 6 and len(word) <= 8:
+            mini_word_list.append(word)
+    return mini_word_list
 
 
 def hard_words(word_list):
-    pass
+    """Given word list, returns a subset list of those words 8 or more characters in size"""
+    mini_word_list = []
+    for word in word_list:
+        if len(word) >= 8:
+            mini_word_list.append(word)
+    return mini_word_list
+
 
 
 def random_word(word_list):
@@ -62,5 +82,5 @@ def is_word_complete(word, lst):
 if __name__ == "__main__":
     word_list = read_file("web2")
     print(word_list[0:7])
-    print(mode_selector())
+    print(size_selector())
     # pass
